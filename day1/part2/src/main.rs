013c2919e9eb7fb05b30c320063d53f0e8acef7d
Fredrik Jonsén <fredrik.jonsen@zenon.se>
2022-12-01 10:27:40 +0100

    Day 1 complete

--- day1/part2/src/main.rs
+++ day1/part2/src/main.rs
@@ -1,13 +1,15 @@
 fn main() {
     let input = include_str!("../../input.txt");
-    let max = input
+    let mut elves = input
         .split("\n\n")
         .map(|e| {
             e.split_whitespace()
                 .map(|i| i.parse::<i64>().expect("Failed to parse item"))
                 .sum::<i64>()
         })
-        .max()
-        .unwrap();
-    println!("{max}")
+        .collect::<Vec<_>>();
+
+    elves.sort_by(|a, b| b.cmp(a));
+    let total = elves.into_iter().take(3).sum::<i64>();
+    println!("{total}")
 }
