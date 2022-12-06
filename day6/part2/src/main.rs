14f854bdba51178b1ac542c17875d4e7fb2fae41
Fredrik Jonsén <fredrik.jonsen@zenon.se>
2022-12-06 11:36:30 +0100

    Day 6

---
+++ day6/part2/src/main.rs
@@ -0,0 +1,47 @@
+fn find_marker(input: &str) -> usize {
+    const MESSAGE_LENGTH: usize = 14;
+    let (intro, rest) = input.split_at(MESSAGE_LENGTH - 1);
+    let mut start = ['0'; MESSAGE_LENGTH];
+    intro
+        .chars()
+        .enumerate()
+        .for_each(|(i, c)| start[i + 1] = c);
+    rest.chars()
+        .scan(start, |state, c| {
+            state.rotate_left(1);
+            state[state.len() - 1] = c;
+            Some(*state)
+        })
+        .enumerate()
+        .find_map(|(i, c)| {
+            for (index, ch) in c.iter().enumerate() {
+                let rest = &c[(index + 1)..];
+                if rest.contains(ch) {
+                    return None;
+                }
+            }
+            Some(i + MESSAGE_LENGTH)
+        })
+        .expect("Didn't find start of marker")
+}
+
+fn main() {
+    let input = include_str!("../../input.txt");
+    println!("{}", find_marker(input));
+}
+
+#[cfg(test)]
+mod tests {
+    use test_case::test_case;
+
+    use crate::find_marker;
+
+    #[test_case("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+    #[test_case("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+    #[test_case("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+    #[test_case("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+    #[test_case("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+    fn test_find_marker(input: &str, index: usize) {
+        assert_eq!(index, find_marker(input));
+    }
+}
